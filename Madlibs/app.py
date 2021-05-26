c679247740eb3a6e830fcfede21cd03f099a2fc7
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2021-05-26 12:09:43 -0400

    new mad libs project added

--- Madlibs/app.py
+++ Madlibs/app.py
@@ -1,1 +1,9 @@
-#Mad Libs project
+# Mad Libs project
+
+color = input("Enter a color:")
+plural_noun = input("Enter a plural noun:")
+celebrity = input("Enter a celebrity name:")
+
+print("Roses are", color)
+print(plural_noun, "are blue")
+print("i love", celebrity)
