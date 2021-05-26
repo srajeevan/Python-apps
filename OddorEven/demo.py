edb926b72152a8bc1875eae7a29a975f29775693
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2021-05-26 12:00:31 -0400

    Odd or Even finished

--- OddorEven/demo.py
+++ OddorEven/demo.py
@@ -1,3 +1,9 @@
-#Odd or Even Project
-
-print("What number are you thinking?")
+# Odd or Even Project
+# print("What number are you thinking?")
+num = input("WHat number are you thinking?")
+mod = int(num) % 2
+if mod > 0:
+    # print("That is an odd number")
+    print("The number you have entered is", num, "and it is odd")
+else:
+    print("That is an even number")
