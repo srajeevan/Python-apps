27f0182c28b320681627466ae3d6ca961b2fc189
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2021-06-10 17:25:42 -0400

    overlap issue

---
+++ ListOverlap/listoverlap.py
@@ -0,0 +1,12 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+overlap = []
+
+for i in a:
+    if i in b and i not in overlap:
+        overlap.append(i)
+
+print(overlap)
+
+
